521edc89700ef99a1f1e94c359531aeb0fe56683
Alexandra <alexandra.holcombe@gmail.com>
2017-04-21 14:18:58 -0700

    Configure app to use static files

--- src/GummiBearKingdom/Startup.cs
+++ src/GummiBearKingdom/Startup.cs
@@ -34,12 +34,15 @@ namespace GummiBearKingdom
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
