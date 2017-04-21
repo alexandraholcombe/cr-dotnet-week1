23f760b86115f19c3a236d87c4bc88cd7f606947
Alexandra <alexandra.holcombe@gmail.com>
2017-04-21 11:06:14 -0700

    Create migrations, change appsettings.json to reflect current versioning

--- src/GummiBearKingdom/Models/GummiBearKingdomDbContext.cs
+++ src/GummiBearKingdom/Models/GummiBearKingdomDbContext.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using GummiBearKingdom.Models;
 
 namespace GummiBearKingdom.Models
 {
-    public class GummiBearKingdomDbContext: DbContext
+    public class GummiBearKingdomDbContext : DbContext
     {
-    }
+        public GummiBearKingdomDbContext()
+        {
+        }
 
-    public DbSet <Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        options.UseSqlServer(@"Server=(localdb)\mssqldb;Database=GummiBearKingdom;integrated security = True");
-    }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=IsabellaBirdBlog;integrated security=True");
+        }
 
-    public GummiBearKingdomDbContext(DbContextOptions<GummiBearKingdomDbContext> options)
-        : base(options)
-    {
-    }
+        public GummiBearKingdomDbContext(DbContextOptions<GummiBearKingdomDbContext> options)
+            : base(options)
+        {
+        }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
